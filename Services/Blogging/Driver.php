7de931f666a25e7c2f0f7a10562cfed8b47364b0
Christian Weiske <cweiske@php.net>
2007-01-27 16:59:40 +0000

    Importing Services_Blogging into cvs
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Blogging/trunk@228196 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Services/Blogging/Driver.php
@@ -0,0 +1,81 @@
+<?php
+require_once 'Services/Blogging/Post.php';
+
+/**
+*   A PHP interface to blogging APIs
+*
+*   @category    Services
+*   @package     Services_Blogging
+*   @author      Anant Narayanan <anant@php.net>
+*   @author      Christian Weiske <cweiske@php.net>
+*   @license     http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+*/
+abstract class Services_Blogging_Driver
+{
+
+    /**
+    *   Error code: Username or password doesn't exist/are wrong
+    */
+    const ERROR_USERIDPASS = 102;
+
+
+
+    /**
+    *   Save a new post into the blog.
+    *
+    *   @param Services_Blogging_Post $post     Post object to put online
+    *
+    *   @return string  The ID assigned to the post
+    */
+    abstract public function savePost(Services_Blogging_Post $post);
+
+
+
+    /**
+    *   Delete a given post
+    *
+    *   @param mixed  $post   Services_Blogging_Post object to delete,
+    *                          or post id (integer) to delete
+    *   @return boolean     True if deleted, false if not.
+    */
+    abstract public function deletePost($post);
+
+
+
+
+    /**
+    *   Returns an array of strings thay define
+    *   the properties that a post to this blog may
+    *   have.
+    *
+    *   @return array   Array of strings
+    */
+    abstract public function getSupportedPostProperties();
+
+
+
+    /**
+    *   Checks if the given property name/id is supported
+    *   for this driver.
+    *
+    *   @param string $strProperty  Property name/id to check
+    *
+    *   @return boolean     If the property is supported
+    */
+    abstract public function isPostPropertySupported($strProperty);
+
+
+
+    /**
+    *   Creates a new post object and returns that.
+    *   Automatically sets the driver object in the post.
+    *
+    *   @return Services_Blogging_Post  New post object
+    */
+    public function createNewPost()
+    {
+        return new Services_Blogging_Post($this);
+    }//public function createNewPost()
+
+}//abstract class Services_Blogging_Driver
+?>
---
+++ Services/Blogging/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*   Exception class for the Services_Blogging package.
+*   Extends the normal exception class to make it easy
+*   to distinguish between blogging and other exceptions
+*   via instanceof
+*
+*   @category    Services
+*   @package     Services_Blogging
+*   @author      Anant Narayanan <anant@php.net>
+*   @author      Christian Weiske <cweiske@php.net>
+*   @license     http://www.gnu.org/copyleft/lesser.html  LGPL License 2.1
+*/
+class Services_Blogging_Exception extends Exception
+{
+    public function __construct($message = null, $code = 0)
+    {
+        parent::__construct($message, $code);
+    }//public function __construct($message = null, $code = 0)
+}//class Services_Blogging_Exception extends Exception
+?>
